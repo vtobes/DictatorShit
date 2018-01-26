8e6dfff87ad49c1ba06dbbc29b9b410cbc00536f
AzureAD\VICTORTOBESPEREZ <vtobes@gmail.com>
2018-01-26 21:33:25 +0100

    Added Follow_mouse

---
+++ Assets/Follow_Mouse.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow_Mouse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+    }
+
+    private void OnMouseDrag()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        transform.position = ray.origin + new Vector3(0, 0, 5);
+
+    }
+    private void OnMouseUp()
+    {
+        Debug.Log("seleccionador");
+    }
+}
---
+++ Assets/infomation_point.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class infomation_point : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+   
+        Debug.Log("Position: "+ ray.direction.ToString());
+
+    }
+}
