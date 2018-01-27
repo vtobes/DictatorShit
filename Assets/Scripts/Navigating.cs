1847bce440e6459c032ff290150026ecc60c792d
mrr0088 <mrr0088@alu.ubu.es>
2018-01-27 10:56:46 +0100

    Navigation Script added

---
+++ Assets/Scripts/Navigating.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navigating : MonoBehaviour {
+
+
+    Vector3 newPosition;
+
+	// Use this for initialization
+	void Start () {
+        newPosition = transform.position;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                newPosition = hit.point;
+                transform.position = newPosition;
+            }
+        }
+	}
+}
