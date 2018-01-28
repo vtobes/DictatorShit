d2f5caaed23fc797beb0cda54806d4edfa01ef0d
AzureAD\VICTORTOBESPEREZ <vtobes@gmail.com>
2018-01-28 13:49:17 +0100

    Persistence improved

---
+++ Assets/Scripts/backToMap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class backToMap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+     
+    }
+    public void loadscene()
+    {
+        SceneManager.LoadScene("MapScene");
+    }
+}
